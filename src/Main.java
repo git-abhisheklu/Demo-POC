96adf1db7cdde555fafaaafee81bfbdc8f86f36b
abhishek gupta <abhishekgupta.lu@gmail.com>
2025-06-05 15:43:47 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main{
+    public static void main(String[] args) {
+        int $pValue=10;
+//        System.out.println($pValue);
+//        List<String> names= Arrays.asList("Alok","Rohan","Suraj","Arpit");
+//        List<List<String>> students=Arrays.asList(
+//                Arrays.asList("Komal", "Payal", " Tarak","karthik"),
+//                Arrays.asList("Alok","Rohan","Suraj","Arpit")
+//        );
+//        List<String> filteredNames=names.stream().filter(name -> name.startsWith("A")).collect(Collectors.toList());
+//        System.out.println(filteredNames);
+        ArrayList<Integer> $list=new ArrayList<>(100);
+        System.out.println($list.size());
+    }
+}
